f8c045d8ad6aaf5566d63070113df65a689ff479
David Denton <denton.david@gmail.com>
2017-06-24 18:49:29 +0100

    playing around

--- src/main/kotlin/org/http4k/todo/ReactApp.kt
+++ src/main/kotlin/org/http4k/todo/ReactApp.kt
@@ -1,13 +1,13 @@
 package org.http4k.todo
 
 import org.http4k.core.ContentType
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.CorsPolicy.Companion.UnsafeGlobalPermissive
 import org.http4k.filter.ServerFilters.Cors
-import org.http4k.routing.ResourceLoader
+import org.http4k.routing.ResourceLoader.Companion.Directory
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.routing.static
@@ -20,9 +20,9 @@ fun main(args: Array<String>) {
     Cors(UnsafeGlobalPermissive)
         .then(routes(
             "/api" bind routes(
-                "/" to Method.GET bind { Response(OK) }
+                "/" to GET bind { Response(OK) }
             ),
-            "/" bind static(ResourceLoader.Directory("src/main/resources"), "jsx" to ContentType.TEXT_PLAIN)
+            "/" bind static(Directory("src/main/resources"), "jsx" to ContentType("text/babel"))
         ))
         .asServer(Jetty(port.toInt())).start().block()
 }
