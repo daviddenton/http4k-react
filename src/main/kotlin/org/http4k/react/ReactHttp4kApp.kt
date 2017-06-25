08f808792c2ec17525edf9cbc4d1b63d3ad760dc
David Denton <denton.david@gmail.com>
2017-06-25 14:02:45 +0100

    tweaks to API

--- src/main/kotlin/org/http4k/react/ReactHttp4kApp.kt
+++ src/main/kotlin/org/http4k/react/ReactHttp4kApp.kt
@@ -14,17 +14,18 @@ import org.http4k.server.asServer
 import java.time.Clock
 import java.time.LocalDateTime
 
+object Auditor {
+    operator fun invoke(clock: Clock = Clock.systemUTC()) = ResponseFilters.ReportLatency { req, resp, duration -> println("${LocalDateTime.now(clock)} - ${req.method} - ${req.uri} - ${resp.status.code} - ${duration.toMillis()}ms") }
+}
+
 object ReactHttp4kApp {
-
-    operator fun invoke(port: Int, resourceLoader: ResourceLoader): Http4kServer {
-        val clock = Clock.systemUTC()
-        val auditor = ResponseFilters.ReportLatency { req, resp, duration -> println("${LocalDateTime.now(clock)} - ${req.method} - ${req.uri} - ${resp.status.code} - ${duration.toMillis()}ms") }
-
-        return Cors(UnsafeGlobalPermissive)
-            .then(routes(
-                "/api" bind auditor.then(Api()),
-                "/" bind static(resourceLoader)
-            ))
+    operator fun invoke(port: Int, resourceLoader: ResourceLoader): Http4kServer =
+        Cors(UnsafeGlobalPermissive)
+            .then(
+                routes(
+                    "/api" bind Auditor().then(Api()),
+                    "/" bind static(resourceLoader)
+                )
+            )
             .asServer(Jetty(port))
-    }
 }
